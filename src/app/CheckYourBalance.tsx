50cbf1d5a5d639d3579d504ee2fb1efc34b7e8a6
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2024-07-09 00:28:37 +0300

    some styles fixed in check your balance component

--- src/app/CheckYourBalance.tsx
+++ src/app/CheckYourBalance.tsx
@@ -1,9 +1,10 @@
-import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { useState } from "react";
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
 export default function CheckBalance() {
   const [loading, setLoading] = useState(false);
   const [balanceMessage, setBalanceMessage] = useState("");
+  const [balanceInSOL, setBalanceInSOL] = useState<number | null>(null); // State olarak balanceInSOL ekleniyor
   const publicKey = new PublicKey("7TesgdXUJvejf3jwKyNNBAG9c8YqjvDYNyt7pQ8ziBmy");
   const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
@@ -12,8 +13,9 @@ export default function CheckBalance() {
     try {
       const balanceInLamports = await connection.getBalance(publicKey);
       const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+      setBalanceInSOL(balanceInSOL); // State olarak balanceInSOL güncelleniyor
       setBalanceMessage(
-        `💰 The balance for the wallet at address ${publicKey} is<br/><span style="font-size: 24px; font-weight: bold; color: #6200ff;">${balanceInSOL} SOL!</span>`
+        `💰 The balance for the wallet at address ${publicKey.toBase58()} is ${balanceInSOL} SOL!`
       );
     } catch (error) {
       console.error("Error fetching balance:", error);
@@ -27,12 +29,21 @@ export default function CheckBalance() {
   };
 
   return (
-    <div>
-      <button onClick={getBalance} disabled={loading} className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-        Check Your Balance {balanceMessage && (
-        <p dangerouslySetInnerHTML={{ __html: balanceMessage }} />
-      )}
+    <>
+      <button className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-5" onClick={getBalance} 
+      disabled={loading}>
+        {(balanceMessage) ? "" : "Check Your Balance"} 
+        <div>
+        {balanceMessage && (
+          <div>
+            Balance at wallet adress: {publicKey.toBase58()} is <span style={{ fontSize: "24px", fontWeight: "bold", color: "#f854c7" }}>
+            {balanceInSOL} SOL
+            </span>
+          </div>
+        )}
+      </div>
       </button>
-    </div>
+      
+    </>
   );
 }
