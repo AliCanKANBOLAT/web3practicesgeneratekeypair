c9375c1b42ae96b83d91be27f3473dcb8313d758
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2024-07-08 21:02:13 +0300

    previous step for generate kaypair added as note

--- src/app/generate-keypair.ts
+++ src/app/generate-keypair.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
+// import { Keypair } from "@solana/web3.js";
+// const keypair = Keypair.generate();
+// console.log(`✅ Generated keypair!`)
+
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
 export default keypair;
